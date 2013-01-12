4b9b83d4d2bee708a5d117ea8e925d34627625bb
zachoz <zach.de.koning@gmail.com>
2013-01-12 16:57:06 +1100

    .info can be run by anyone.

--- src/com/zachoz/OresomeBot/commands/infoCommand.java
+++ src/com/zachoz/OresomeBot/commands/infoCommand.java
@@ -9,16 +9,10 @@ import com.zachoz.OresomeBot.Config;
 public class InfoCommand extends ListenerAdapter {
 
     public void onMessage(MessageEvent event) {
-        if (event.getMessage().equals(".info")) {
-            String admin = "";
-	      for (int i = 0 ; i < Config.admins.length; i++ ) {
-		  admin += Config.admins[i];
-	      }
-	      
-	  
-		  if(admin.contains(event.getUser().getNick())) { 
-                event.respond("Hi, I'm OresomeBot, an IRC bot designed by Zachoz for primary use in OresomeCraft IRC channels. I'm written in Java and use the PircBotx Library. I also have various useful administrative and user commands.");
+	if (event.getMessage().equals(".info")) {
+
+	    event.respond("Hi, I'm OresomeBot, an IRC bot designed by Zachoz for primary use in OresomeCraft IRC channels. I'm written in Java and use the PircBotx Library. I also have various useful administrative and user commands.");
+
 	}
     }
-    }
-    }
+}
