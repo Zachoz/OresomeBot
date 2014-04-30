c00ffdd1e5e3167a4c8f08594ed9040682878527
Zach de Koning <zach.de.koning@gmail.com>
2014-04-30 20:36:10 +1000

    Oops, might want to remove those

--- src/com/zachoz/OresomeBot/commands/MiscCommands.java
+++ src/com/zachoz/OresomeBot/commands/MiscCommands.java
@@ -3,8 +3,6 @@ package com.zachoz.OresomeBot.commands;
 import com.zachoz.OresomeBot.core.ChannelCommand;
 import com.zachoz.OresomeBot.core.PermissionLevel;
 import com.zachoz.OresomeBot.core.PrivateMessageCommand;
-import com.zachoz.OresomeBot.oresomecraft.forums.ForumManager;
-import com.zachoz.OresomeBot.oresomecraft.forums.NodeView;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PrivateMessageEvent;
 
@@ -12,7 +10,7 @@ public class MiscCommands {
 
     @ChannelCommand(name = "ping",
             description = "Sends back pong!",
-            permissionLevel = PermissionLevel.OPERATOR)
+            permissionLevel = PermissionLevel.REGULAR)
     public static void ping(MessageEvent event, String[] args) {
         event.respond("pong!");
     }
@@ -22,20 +20,6 @@ public class MiscCommands {
             permissionLevel = PermissionLevel.REGULAR)
     public static void ping(PrivateMessageEvent event, String[] args) {
         event.respond("pong!");
-    }
-
-    @ChannelCommand(name = "debug",
-            description = "Sends back pong!",
-            permissionLevel = PermissionLevel.REGULAR)
-    public static void debug(MessageEvent event, String[] args) {
-        ForumManager.generalChat = new NodeView(2).build();
-    }
-
-    @ChannelCommand(name = "debug1",
-            description = "Sends back pong!",
-            permissionLevel = PermissionLevel.REGULAR)
-    public static void debug1(MessageEvent event, String[] args) {
-        event.respond(ForumManager.newThread(ForumManager.generalChat, new NodeView(2).build()));
     }
 
 }
